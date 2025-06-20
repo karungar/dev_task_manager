db9c8c9c405710cc017778ea79912805b9074d77
okwareddevnest <softengdedan@gmail.com>
2025-06-20 21:56:42 +0300

    refactor: return token as an object in signup and login responses

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,7 +16,7 @@ exports.signup = async (req, res) => {
     const token = jwt.sign({ id: user._id, role: user.role}, process.env.JWT_SECRET, {
         expiresIn: '1h'
     });
-    res.json(token);
+    res.json({ token });
 };
 
 // Login Endpoint Logic
@@ -32,5 +32,5 @@ exports.login = async (req, res) => {
     const token = jwt.sign({ id: user._id, role: user.role}, process.env.JWT_SECRET, {
         expiresIn: '1h'
     });
-    res.json(token);
+    res.json({ token });
 }
