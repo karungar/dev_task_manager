58d9309d971605a455ef3bccf506630ee93b7778
okwareddevnest <softengdedan@gmail.com>
2025-06-20 15:55:57 +0300

    backend fully done

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 // Connects to MongoDB using mongoose
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("MongoDB connected successfully");
     } catch (error) {
         console.error("MongoDb connection failed: ", error.message);
---
+++ server/controllers/taskController.js
@@ -0,0 +1,19 @@
+const Task = require("../models/Task");
+
+// POST /api/tasks
+exports.createTask = async (req, res)=> {
+    const task = await Task.create({ ...req.body, owner: req.user.id});
+    res.json(task);
+};
+
+// GET /api/tasks/me
+exports.getMyTasks = async (req, res) => {
+    const tasks = await Task.find({ owner: req.user.id });
+    res.json(tasks);
+};
+
+// GET /api/tasks/all
+exports.getAllTasks = async (req, res) => {
+    const tasks = await Task.find().poplulate("owner", "email");
+    res.json(tasks);
+};
