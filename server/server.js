72c6bd1d4d026511370cf5db8cb1d531a38c56a2
Sylvia <sylvia.waweru@gmail.com>
2025-07-18 20:51:14 +0300

    cors configured

--- server/server.js
+++ server/server.js
@@ -6,8 +6,26 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-app.use(cors());
+const allowedOrigins = [
+    'http://localhost:5173',            //local dev
+    'https://devtaskmanager.vercel.app' //Production
+];
+
+app.use(
+    cors({
+        origin: (origin, cb) => {
+            //Allow Postman/curl which send no origin
+            if (!origin ||allowedOrigins.includes(origin)) return cb(null. true);
+        },
+        credentials: true,
+        methods: 'GET,POST,PUT,DELETE',
+        allowedHeaders: 'content-Type, Authorization',
+
+
+    })); 
 app.use(express.json());
+
+
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
